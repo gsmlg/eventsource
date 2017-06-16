a94330646dcbb221a8090eb9fd3fb0763c0c27ef
gaoshiming <gaoshiming@zdns.cn>
2017-06-16 10:29:24 +0800

    systeminfo

--- server/systeminformation.js
+++ server/systeminformation.js
@@ -1,7 +1,13 @@
 var util = require('util');
 var si = require('systeminformation');
 
+var displayOptions = {depth: 10, colors: false};
+
 // promises style - new in version 3
 si.getStaticData()
-  .then(data => console.log(util.inspect(data, {depth: 10, colors: true})))
+  .then(data => console.log(util.inspect(data, displayOptions)))
+  .catch(error => console.error(error));
+
+si.getDynamicData()
+  .then(data => console.log(util.inspect(data, displayOptions)))
   .catch(error => console.error(error));
